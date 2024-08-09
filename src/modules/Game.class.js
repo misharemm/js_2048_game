7c4df8260f1d79149a5c2a0fc207b0ddb4045e3c
Misha Remeslo <misha.remeslol@gmail.com>
2024-08-09 18:13:09 +0300

    Test

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -21,6 +21,10 @@ class Game {
    * initial state.
    */
   constructor(initialState) {
+    this.board = initialState;
+    this.score = 0;
+    this.status = 'idle';
+
     // eslint-disable-next-line no-console
     console.log(initialState);
   }
@@ -55,7 +59,50 @@ class Game {
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.board = this.createBoard();
+    this.score = 0;
+    this.status = 'playing';
+    this.addNewCell();
+    console.log('Start works');
+  }
+
+  createBoard() {
+    const board = [];
+
+    for (let i = 0; i < 4; i++) {
+      const row = [];
+
+      for (let j = 0; j < 4; j++) {
+        row.push(0);
+      }
+
+      board.push(row);
+    }
+
+    return board;
+  }
+
+  addNewCell() {
+    const cells = [];
+
+    // eslint-disable-next-line no-shadow
+    for (let row = 0; row < 4; row++) {
+      // eslint-disable-next-line no-shadow
+      for (let col = 0; col < 4; col++) {
+        if (this.board[row][col] === 0) {
+          cells.push({ row, col });
+        }
+      }
+    }
+
+    const randomCell = Math.floor(Math.random() * cells.length);
+    const { row, col } = cells[randomCell];
+
+    const randomNum = Math.random() < 0.8 ? 2 : 4;
+
+    this.board[row][col] = randomNum;
+  }
 
   /**
    * Resets the game.
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,18 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const start = document.querySelector('.button .start');
+const field = document.querySelector('.game-field');
+const score = document.querySelector('.game-score');
+const message = {
+  lose: document.querySelector('message-lose'),
+  win: document.querySelector('message-win'),
+  start: document.querySelector('message-start'),
+};
+
+start.addEventListener('click', () => {
+  game.start();
+});
